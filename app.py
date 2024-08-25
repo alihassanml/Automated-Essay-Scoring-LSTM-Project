9a36147a10c3ccfe7a9ce37730e3bdaad60514a5
alihassanml <fa-22-bscs-294@lgu.edu.pk>
2024-08-25 23:36:50 +0500

    First Commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import streamlit as st
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
+from spellchecker import SpellChecker
 import pickle
 model = load_model('model.h5')
 
@@ -17,13 +18,31 @@ def predict(text):
     predicted = np.argmax(np.array(predicted), axis=1)
     return predicted
 
+
+def find_incorrect_spelling(text):
+    spell = SpellChecker()
+    words = text.split()
+    misspelled = spell.unknown(words)
+    
+    if misspelled:
+        st.caption(f'Spelling Mistake : {len(misspelled)}')
+        st.caption(f'Incorrect Spelling : {misspelled}')
+    else:
+        st.caption(f'Spelling Free Essay')
+
+
+
+
 def main():
     st.title("Automated Essay Scoring System")
     
     text = st.text_area("Enter your essay text here:")
+    length = len(text.split())
     
     if st.button("Predict"):
         if text:
+            st.caption(f'Length of Essay {length} Words')
+            find_incorrect_spelling(text)
             prediction = predict(text)
             st.success(f"Predicted Score: {prediction[0]}")
         else:
